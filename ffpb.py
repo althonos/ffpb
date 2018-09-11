37f550be172674be395b8e149c78be96810cdad6
Martin Larralde <martin.larralde@ens-cachan.fr>
2018-09-11 10:21:08 +0200

    Switch progress bar library to `tqdm` and display frame count instead of time duration

--- ffpb.py
+++ ffpb.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# Copyright (c) 2017 Martin Larralde (martin.larralde@ens-cachan.fr)
+# Copyright (c) 2017-2018 Martin Larralde (martin.larralde@ens-cachan.fr)
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -21,6 +21,9 @@
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 # OR OTHER DEALINGS IN THE SOFTWARE.
 
+"""A progress bar for `ffmpeg` using `tqdm`.
+"""
+
 from __future__ import unicode_literals
 from __future__ import print_function
 
@@ -39,138 +42,134 @@ else:
 
 import chardet
 import sh
-import progressbar
+import tqdm
 
 
-class AbsoluteTimeETA(progressbar.AbsoluteETA):
-
-    def _calculate_eta(self, progress, data, value, elapsed):
-        return elapsed + progressbar.ETA._calculate_eta(
-            self, progress, data, value, elapsed)
-
-
-class ProgressNotifier(collections.Callable):
+class ProgressNotifier(collections.abc.Callable):
 
     _DURATION_RX = re.compile("Duration: (\d{2}):(\d{2}):(\d{2})\.\d{2}")
     _PROGRESS_RX = re.compile("time=(\d{2}):(\d{2}):(\d{2})\.\d{2}")
     _SOURCE_RX = re.compile("from '(.*)':")
+    _FPS_RX = re.compile("(\d{2}\.\d{2}) fps")
 
     @staticmethod
     def _seconds(hours, minutes, seconds):
-        return (int(hours)*60 + int(minutes))*60 + int(seconds)
+        return (int(hours) * 60 + int(minutes)) * 60 + int(seconds)
 
-    def __init__(self):
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.pbar is not None:
+            self.pbar.close()
+
+    def __init__(self, file=None):
         self.lines = []
         self.line_acc = []
         self.duration = None
         self.source = None
         self.started = False
-        print()
-        self.pbar = progressbar.ProgressBar(widgets=[
-            lambda w, d: os.path.basename(self.source), ' ',
-            progressbar.AnimatedMarker(
-                markers="—/|\\",
-            ),
-            progressbar.Bar(
-                marker='█',
-                fill='░',
-                left=' ╢',
-                right='╟ ',
-            ),
-            progressbar.Percentage(
-                '%(percentage)3d% % '
-            ),
-            progressbar.Timer(
-                format='(%(elapsed)s / '
-            ),
-            AbsoluteTimeETA(
-                format='%(eta)s)',
-                format_finished='%(elapsed)s',
-                format_not_started='--:--:--'
-            ),
-        ])
-
-    def newline(self):
-        self.lines.append(''.join(self.line_acc))
-        self.line_acc = []
+        self.pbar = None
+        self.fps = None
+        self.file = file or sys.stderr
 
     def __call__(self, char, stdin):
 
-        if type(char) != unicode:
-            encoding = chardet.detect(char)['encoding']
+        if not isinstance(char, unicode):
+            encoding = chardet.detect(char)["encoding"]
             char = unicode(char, encoding)
 
-        if char not in '\r\n':
+        if char in "\r\n":
+            line = self.newline()
+            if self.duration is None:
+                self.duration = self.get_duration(line)
+            if self.source is None:
+                self.source = self.get_source(line)
+            if self.fps is None:
+                self.fps = self.get_fps(line)
+            self.progress(line)
+        else:
             self.line_acc.append(char)
-            if self.line_acc[-6:] == list('[y/N] '):
-                print(''.join(self.line_acc), end='')
-                stdin.put(input()+'\n')
+            if self.line_acc[-6:] == list("[y/N] "):
+                print("".join(self.line_acc), end="")
+                stdin.put(input() + "\n")
                 self.newline()
-            return
 
-        self.newline()
+    def newline(self):
+        line = "".join(self.line_acc)
+        self.lines.append(line)
+        self.line_acc = []
+        return line
 
-        self.get_duration(self.lines[-1])
-        self.get_source(self.lines[-1])
-        self.get_progress(self.lines[-1])
+    def get_fps(self, line):
+        """
+        """
+        search = self._FPS_RX.search(line)
+        if search is not None:
+            return round(float(search.group(1)))
 
     def get_duration(self, line):
         search = self._DURATION_RX.search(line)
         if search is not None:
-            self.duration = self._seconds(*search.groups())
+            return self._seconds(*search.groups())
+        return None
 
     def get_source(self, line):
         search = self._SOURCE_RX.search(line)
         if search is not None:
-            self.source = search.group(1)
+            return os.path.basename(search.group(1))
+        return None
 
-    def get_progress(self, line):
+    def progress(self, line):
         search = self._PROGRESS_RX.search(line)
         if search is not None:
-            current = self._seconds(*search.groups())
+            if self.pbar is None:
+                self.pbar = tqdm.tqdm(
+                    desc=self.source,
+                    file=self.file,
+                    total=self.duration * self.fps,
+                    dynamic_ncols=True,
+                    unit=" frames",
+                    ncols=0,
+                )
+            current = self._seconds(*search.groups()) * self.fps
+            self.pbar.update(current - self.pbar.n)
 
-            if not self.started:
-                self.pbar.start(max_value=self.duration)
-                self.started = True
 
-            self.pbar.update(current)
-
-
-def main(argv=None):
+def main(argv=None, stream=sys.stderr):
     argv = argv or sys.argv[1:]
 
-    if {'-h', '-help', '--help'}.intersection(argv):
+    if {"-h", "-help", "--help"}.intersection(argv):
         sh.ffmpeg(help=True, _fg=True)
         return 0
 
-    notifier = ProgressNotifier()
-
     try:
 
-        sh.ffmpeg(
-            sys.argv[1:],
-            _in=queue.Queue(),
-            _err=notifier,
-            _out_bufsize=0,
-            _err_bufsize=0,
-            #_in_bufsize=0,
-            _no_out=True,
-            _no_pipe=True,
-            _tty_in=True,
-            #_fg=True,
-            #_bg=True,
-        )
+        with ProgressNotifier(file=stream) as notifier:
+
+            sh.ffmpeg(
+                sys.argv[1:],
+                _in=queue.Queue(),
+                _err=notifier,
+                _out_bufsize=0,
+                _err_bufsize=0,
+                # _in_bufsize=0,
+                _no_out=True,
+                _no_pipe=True,
+                _tty_in=True,
+                # _fg=True,
+                # _bg=True,
+            )
 
     except sh.ErrorReturnCode as err:
-        print(notifier.lines[-1])
+        print(notifier.lines[-1], file=stream)
         return err.exit_code
 
     except KeyboardInterrupt:
-        print('\nExiting.')
-        return signal.SIGINT + 128    # POSIX standard
+        print("Exiting.", file=stream)
+        return signal.SIGINT + 128  # POSIX standard
 
     else:
-        print()
         return 0
 
 
