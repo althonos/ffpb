987ac63f2237d48d096893aa59ac9a43d95afc66
kubinka0505 <30776937+kubinka0505@users.noreply.github.com>
2022-05-05 12:01:39 +0200

    Update ffpb.py

--- ffpb.py
+++ ffpb.py
@@ -21,160 +21,190 @@
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 # OR OTHER DEALINGS IN THE SOFTWARE.
 
-"""A progress bar for `ffmpeg` using `tqdm`.
-"""
+"""FFmpeg Progress Bar
+
+A progress bar for `FFmpeg` using `TQDM`."""
+
+#-=-=-=-#
 
 from __future__ import unicode_literals
 from __future__ import print_function
 
-import locale
-import os
-import re
-import signal
 import sys
+import os, re
+import locale
+import signal
 import subprocess
+from pathlib import Path
 
 if sys.version_info < (3, 0):
-    import Queue as queue
-    input = raw_input
+	import Queue as queue
+	input = raw_input
 else:
-    import queue
-    unicode = str
+	import queue
+	unicode = str
 
 from tqdm import tqdm
 
+#-=-=-=-#
+
+__author__	= "althonos"
+__credits__	= __author__
+__version__	= "0.4.2"
+
+#-=-=-=-#
+
+def Detect_Binary(Name: str.lower, Variable_Name: str = "PATH", Format: str = "EXE") -> str:
+	"""Looks for binary of `Name` in the `Variable` environment variable."""
+	Name_Original = Name
+	Name = Name.lower()
+	Format = Format.lower()
+	#-=-=-=-#
+	if os.sys.platform.lower().startswith("win"):
+		for Variable in os.environ[Variable_Name].split(os.pathsep):
+			if Name + "." + Format in Variable:
+				# Fix the final path
+				Name = str(Path(Variable).resolve())
+				if not os.path.exists(Name):
+					raise AttributeError(Name_Original + " not found!")
+	#-=-=-=-#
+	return Name
+
+FFmpeg = Detect_Binary("FFmpeg")
+
+#-=-=-=-#
 
 class ProgressNotifier(object):
+	_DURATION_RX = re.compile(b"Duration: (\d{2}):(\d{2}):(\d{2})\.\d{2}")
+	_PROGRESS_RX = re.compile(b"time=(\d{2}):(\d{2}):(\d{2})\.\d{2}")
+	_SOURCE_RX = re.compile(b"from '(.*)':")
+	_FPS_RX = re.compile(b"(\d{2}\.\d{2}|\d{2}) fps")
 
-    _DURATION_RX = re.compile(b"Duration: (\d{2}):(\d{2}):(\d{2})\.\d{2}")
-    _PROGRESS_RX = re.compile(b"time=(\d{2}):(\d{2}):(\d{2})\.\d{2}")
-    _SOURCE_RX = re.compile(b"from '(.*)':")
-    _FPS_RX = re.compile(b"(\d{2}\.\d{2}|\d{2}) fps")
+	@staticmethod
+	def _seconds(hours, minutes, seconds):
+		return (int(hours) * 60 + int(minutes)) * 60 + int(seconds)
 
-    @staticmethod
-    def _seconds(hours, minutes, seconds):
-        return (int(hours) * 60 + int(minutes)) * 60 + int(seconds)
+	def __enter__(self):
+		return self
 
-    def __enter__(self):
-        return self
+	def __exit__(self, exc_type, exc_value, traceback):
+		if self.pbar is not None:
+			self.pbar.close()
 
-    def __exit__(self, exc_type, exc_value, traceback):
-        if self.pbar is not None:
-            self.pbar.close()
+	def __init__(self, file = None, encoding = None, tqdm = tqdm):
+		self.lines = []
+		self.line_acc = bytearray()
+		self.duration = None
+		self.source = None
+		self.started = 0
+		self.pbar = None
+		self.fps = None
+		self.file = file or sys.stderr
+		self.encoding = encoding or locale.getpreferredencoding() or "U8"
+		self.tqdm = tqdm
 
-    def __init__(self, file=None, encoding=None, tqdm=tqdm):
-        self.lines = []
-        self.line_acc = bytearray()
-        self.duration = None
-        self.source = None
-        self.started = False
-        self.pbar = None
-        self.fps = None
-        self.file = file or sys.stderr
-        self.encoding = encoding or locale.getpreferredencoding() or 'UTF-8'
-        self.tqdm = tqdm
+	def __call__(self, char, stdin = None):
+		if isinstance(char, unicode):
+			char = char.encode('ascii')
+		if char in b"\r\n":
+			line = self.newline()
+			if self.duration is None:
+				self.duration = self.get_duration(line)
+			if self.source is None:
+				self.source = self.get_source(line)
+			if self.fps is None:
+				self.fps = self.get_fps(line)
+			self.progress(line)
+		else:
+			self.line_acc.extend(char)
+			if self.line_acc[-6:] == bytearray(b"[y/N] "):
+				print(self.line_acc.decode(self.encoding), end = "", file = self.file)
+				self.file.flush()
+				if stdin:
+					stdin.put(input() + "\n")
+				self.newline()
 
-    def __call__(self, char, stdin = None):
-        if isinstance(char, unicode):
-            char = char.encode('ascii')
-        if char in b"\r\n":
-            line = self.newline()
-            if self.duration is None:
-                self.duration = self.get_duration(line)
-            if self.source is None:
-                self.source = self.get_source(line)
-            if self.fps is None:
-                self.fps = self.get_fps(line)
-            self.progress(line)
-        else:
-            self.line_acc.extend(char)
-            if self.line_acc[-6:] == bytearray(b"[y/N] "):
-                print(self.line_acc.decode(self.encoding), end="", file=self.file)
-                self.file.flush()
-                if stdin:
-                    stdin.put(input() + "\n")
-                self.newline()
+	def newline(self):
+		line = bytes(self.line_acc)
+		self.lines.append(line)
+		self.line_acc = bytearray()
+		return line
 
-    def newline(self):
-        line = bytes(self.line_acc)
-        self.lines.append(line)
-        self.line_acc = bytearray()
-        return line
+	def get_fps(self, line):
+		search = self._FPS_RX.search(line)
+		if search is not None:
+			return round(float(search.group(1)))
+		return None
 
-    def get_fps(self, line):
-        search = self._FPS_RX.search(line)
-        if search is not None:
-            return round(float(search.group(1)))
-        return None
+	def get_duration(self, line):
+		search = self._DURATION_RX.search(line)
+		if search is not None:
+			return self._seconds(*search.groups())
+		return None
 
-    def get_duration(self, line):
-        search = self._DURATION_RX.search(line)
-        if search is not None:
-            return self._seconds(*search.groups())
-        return None
+	def get_source(self, line):
+		search = self._SOURCE_RX.search(line)
+		if search is not None:
+			return os.path.basename(search.group(1).decode(self.encoding))
+		return None
 
-    def get_source(self, line):
-        search = self._SOURCE_RX.search(line)
-        if search is not None:
-            return os.path.basename(search.group(1).decode(self.encoding))
-        return None
+	def progress(self, line):
+		search = self._PROGRESS_RX.search(line)
+		if search is not None:
 
-    def progress(self, line):
-        search = self._PROGRESS_RX.search(line)
-        if search is not None:
+			total = self.duration
+			current = self._seconds(*search.groups())
+			unit = " seconds"
 
-            total = self.duration
-            current = self._seconds(*search.groups())
-            unit = " seconds"
+			if self.fps is not None:
+				unit = " frames"
+				current *= self.fps
+				if total:
+					total *= self.fps
 
-            if self.fps is not None:
-                unit = " frames"
-                current *= self.fps
-                if total:
-                    total *= self.fps
+			if self.pbar is None:
+				self.pbar = self.tqdm(
+					desc = self.source,
+					file = self.file,
+					total = total,
+					dynamic_ncols = 1,
+					unit = unit,
+					ncols = 0,
+					ascii = os.name == "nt" # Windows CMD has problems with Unicode
+				)
 
-            if self.pbar is None:
-                self.pbar = self.tqdm(
-                    desc=self.source,
-                    file=self.file,
-                    total=total,
-                    dynamic_ncols=True,
-                    unit=unit,
-                    ncols=0,
-                    ascii=os.name=="nt",  # windows cmd has problems with unicode
-                )
+			self.pbar.update(current - self.pbar.n)
 
-            self.pbar.update(current - self.pbar.n)
+#-=-=-=-#
 
-def main(argv=None, stream=sys.stderr, encoding=None, tqdm=tqdm):
-    argv = argv or sys.argv[1:]
+def main(argv = None, stream = sys.stderr, encoding = None, tqdm = tqdm):
+	argv = argv or sys.argv[1:]
+	try:
+		with ProgressNotifier(file = stream, encoding = encoding, tqdm = tqdm) as notifier:
+			cmd = [FFmpeg] + argv
+			p = subprocess.Popen(cmd, stderr = subprocess.PIPE)
 
-    try:
-        with ProgressNotifier(file=stream, encoding=encoding, tqdm=tqdm) as notifier:
+			while 1:
+				out = p.stderr.read(1)
+				if out == b"" and p.poll() != None:
+					break
+				if out != b"":
+					notifier(out)
 
-            cmd = ["ffmpeg"] + argv
-            p = subprocess.Popen(cmd, stderr=subprocess.PIPE)
+	except KeyboardInterrupt:
+		print("Exiting.", file = stream)
+		return signal.SIGINT + 128 # POSIX standard
 
-            while True:
-                out = p.stderr.read(1)
-                if out == b"" and p.poll() != None:
-                    break
-                if out != b"":
-                    notifier(out)
+	except Exception as err:
+		print("Unexpected exception:", err, file = stream)
+		return 1
 
-    except KeyboardInterrupt:
-        print("Exiting.", file=stream)
-        return signal.SIGINT + 128  # POSIX standard
+	else:
+		if p.returncode != 0:
+			print(notifier.lines[-1].decode(notifier.encoding), file = stream)
+		return p.returncode
 
-    except Exception as err:
-        print("Unexpected exception:", err, file=stream)
-        return 1
-
-    else:
-        if p.returncode != 0:
-            print(notifier.lines[-1].decode(notifier.encoding), file=stream)
-        return p.returncode
-
+#-=-=-=-#
 
 if __name__ == "__main__":
-    sys.exit(main())
+	sys.exit(main())
