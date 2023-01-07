d9c4e13f1753d0f9afca62cb8ca7fcaf4a2e154e
kubinka0505 <30776937+kubinka0505@users.noreply.github.com>
2023-01-07 20:36:35 +0100

    Update deploy.ps1

--- ci/appveyor/deploy.ps1
+++ ci/appveyor/deploy.ps1
@@ -1,7 +1,7 @@
 If ($env:PYTHON_VERSION -eq "3.9") {
-    If ($env:APPVEYOR_REPO_TAG -eq "true") {
-        Invoke-Expression "$env:PYTHON\\python.exe -m twine upload --skip-existing dist\\*"
-    } Else {
-        write-output "Not on a tag on master, won't deploy to PyPI"
-    }
+	If ($env:APPVEYOR_REPO_TAG -eq "true") {
+		Invoke-Expression "$env:PYTHON\\python.exe -m twine upload --skip-existing dist\\*"
+	} Else {
+		write-output "Not on a tag on master, won't deploy to PyPI"
+	}
 }
